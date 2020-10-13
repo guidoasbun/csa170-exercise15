58382138fd3b458d4e8e975513f983fe7ee60f98
guidoasbun <gdrodrigo8@gmail.com>
2020-10-12 18:59:12 -0700

    Stars done

--- src/Stars.java
+++ src/Stars.java
@@ -6,8 +6,20 @@
  *   Exercise 13
  */
 
+import java.util.Scanner;
+
 public class Stars {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
+        System.out.print("Enter an integer from 3 to 10: ");
+        int num = in.nextInt();
+
+        for(int i = 0; i < num; i++){
+            for(int j = num; j > i; j--){
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
     }
 }
